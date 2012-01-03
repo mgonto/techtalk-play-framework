fd32938d668efda05c6f792dcafcf4e855408159
Martin Gontovnikas <gonto@zauberlabs.com>
2012-01-03 15:09:02 -0300

    Agregada la busqueda

--- agenda/app/controllers/Agenda.java
+++ agenda/app/controllers/Agenda.java
@@ -7,6 +7,8 @@ import play.data.validation.Valid;
 import play.mvc.Controller;
 import play.mvc.With;
 
+import com.google.common.collect.Lists;
+
 @With(Secure.class)
 public class Agenda extends Controller {
     
@@ -27,6 +29,15 @@ public class Agenda extends Controller {
     public static void list() {
         List<Contact> contacts = Contact.findAll();
         render(contacts);
+    }
+    
+    public static void search(final String q, final boolean first) {
+        List<Contact> contacts = Lists.newArrayList();
+        if (!q.isEmpty()) {
+            contacts = Contact.find("byNameLike", "%" + q + "%").fetch();
+        }
+        params.flash("q");
+        render(contacts, first);
     }
 
 }
